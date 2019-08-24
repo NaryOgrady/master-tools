20b4e92ee2152850de2733bc65bf6daedd28e52b
NaryOgrady <josel.garza@outlook.com>
2019-08-24 17:17:49 -0500

    Add route for home page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
   configureRouter(config, router) {
     config.title = 'Master Tools';
+    config.map([
+      {
+        route: [''], name: 'home', moduleId: PLATFORM.moduleName('./home/home'), nav: true, title: 'Welcome'
+      }
+    ]);
 
     this.router = router;
   }
